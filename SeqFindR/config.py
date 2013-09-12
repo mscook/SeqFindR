f54824d944f8a18abab73d96f41afadb76fc6b67
Mitchell Stanton-Cook <mscook@gmail.com>
2013-09-12 10:47:59 +1000

    Minor formatting changes

--- SeqFindR/config.py
+++ SeqFindR/config.py
@@ -11,6 +11,10 @@
 #     or implied. See the License for the specific language governing
 #     permissions and limitations under the License.
 
+"""
+SeqFindR configuration class
+"""
+
 import os
 import sys
 
