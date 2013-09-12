3478c51b7b3fd691f49baf29f48bc3d37b60e7bf
Mitchell Stanton-Cook <mscook@gmail.com>
2013-09-12 11:45:55 +1000

    Shell script to generate examples

---
+++ example/run_examples.sh
@@ -0,0 +1,4 @@
+SeqFindR Antibiotic_markers.fa assemblies/ -o run1 -l 
+SeqFindR Antibiotic_markers.fa assemblies/ -m consensus/ -o run2 -l
+SeqFindR Antibiotic_markers.fa assemblies/ -m consensus/ -o run3 -l -r
+SeqFindR Antibiotic_markers.fa assemblies/ -m consensus/ -o run4 -l -r --index_file dummy.order
