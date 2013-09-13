1c216234fedef2c79054bf74a7ee3a901d044237
Mitchell Stanton-Cook <mscook@gmail.com>
2013-09-13 13:03:33 +1000

    Streamlined testing

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import sys 
-sys.path.append('../')
-import doctest
+import sys, os, doctest 
+
+sys.path.insert(0, "../")
 
 doctest.testfile("blast.tests")
 doctest.testfile("imaging.tests")
