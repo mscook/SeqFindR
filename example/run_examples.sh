0e01d83885fc8eb945b79e07155704d1a7637c8e
Mitchell Stanton-Cook <mscook@gmail.com>
2014-03-06 15:59:49 +1000

    Add example for running with careful

--- example/run_examples.sh
+++ example/run_examples.sh
@@ -2,3 +2,4 @@ SeqFindR Antibiotic_markers.fa assemblies/ -o run1 -l
 SeqFindR Antibiotic_markers.fa assemblies/ -m consensus/ -o run2 -l
 SeqFindR Antibiotic_markers.fa assemblies/ -m consensus/ -o run3 -l -r
 SeqFindR Antibiotic_markers.fa assemblies/ -m consensus/ -o run4 -l -r --index_file dummy.order
+SeqFindR Antibiotic_markers.fa assemblies/ -o run5 -l --careful 0.1
