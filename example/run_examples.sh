fe7fd161116a6b13f7ecd73eb278818dc03a024c
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-07-10 09:59:52 +1000

    Make em runnable

--- example/run_examples.sh
+++ example/run_examples.sh
@@ -1,5 +1,5 @@
-SeqFindR Antibiotic_markers.fa assemblies/ -o run1 -l 
-SeqFindR Antibiotic_markers.fa assemblies/ -m consensus/ -o run2 -l
-SeqFindR Antibiotic_markers.fa assemblies/ -m consensus/ -o run3 -l -r
-SeqFindR Antibiotic_markers.fa assemblies/ -m consensus/ -o run4 -l -r --index_file dummy.order
-SeqFindR Antibiotic_markers.fa assemblies/ -o run5 -l --careful 0.1
+SeqFindr Antibiotic_markers.fa assemblies/ -o run1 -l 
+SeqFindr Antibiotic_markers.fa assemblies/ -m consensus/ -o run2 -l
+SeqFindr Antibiotic_markers.fa assemblies/ -m consensus/ -o run3 -l -r
+SeqFindr Antibiotic_markers.fa assemblies/ -m consensus/ -o run4 -l -r --index_file dummy.order
+SeqFindr Antibiotic_markers.fa assemblies/ -o run5 -l --careful 0.1
