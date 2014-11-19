eba80ff5cf9277b77eb745aaecb5a01ec7696e2c
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-11-19 11:09:28 +1000

    Initial BLAST tests

--- tests/unittests/test_blast.py
+++ tests/unittests/test_blast.py
@@ -1,1 +1,35 @@
 from context import blast
+from context import pytest
+
+
+def test_make_BLAST_database():
+    """
+    Test the make_BLAST_database() function
+
+    Function signature::
+
+    make_BLAST_database(fasta_file)
+    """
+    pass
+
+
+def test_run_BLAST():
+    """
+    Test the run_BLAST() function
+
+    Function signature::
+
+        run_BLAST(query, database, args)
+    """
+    pass
+
+
+def parse_BLAST():
+    """
+    Test the parse_BLAST() function
+
+    Function signature::
+
+        parse_BLAST(blast_results, tol, careful)
+    """
+    pass
