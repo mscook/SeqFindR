94b037e5d01f4a56b9a83cce0005fb5e3c14d267
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-11-20 10:30:17 +1000

    Added tests for make_BLAST_database()

--- tests/unittests/test_blast.py
+++ tests/unittests/test_blast.py
@@ -1,8 +1,10 @@
 from context import blast
 from context import pytest
+import os
+import glob
 
 
-def test_make_BLAST_database():
+def test_make_BLAST_database(tmpdir):
     """
     Test the make_BLAST_database() function
 
@@ -10,7 +12,20 @@ def test_make_BLAST_database():
 
     make_BLAST_database(fasta_file)
     """
-    pass
+    fasta = os.path.abspath("../../example/Antibiotic_markers.fa")
+    tmpdir = str(tmpdir)
+    os.chdir(str(tmpdir))
+    os.mkdir("DBs")
+    blast.make_BLAST_database(fasta)
+    infs = glob.glob(os.path.join(tmpdir, "DBs")+"/*")
+    tidied = []
+    for f in infs:
+        tidied.append(f.split('/')[-1])
+    assert len(infs) == 4
+    assert "Antibiotic_markers.fa.nhr" in tidied
+    assert "Antibiotic_markers.fa.nin" in tidied
+    assert "Antibiotic_markers.fa.nsq" in tidied
+    assert "Antibiotic_markers.fa" in tidied
 
 
 def test_run_BLAST():
@@ -24,7 +39,7 @@ def test_run_BLAST():
     pass
 
 
-def parse_BLAST():
+def test_parse_BLAST():
     """
     Test the parse_BLAST() function
 
